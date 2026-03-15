1d0a71440c07256b6a1eb8001a4f540306f06c00
Jose Dalanon <josedalanon630@gmail.com>
2026-03-15 19:32:54 +0800

    Setup model

---
+++ app/Models/Post.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /** @use HasFactory<\Database\Factories\PostFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        "title",
+        "slug",
+        "content",
+        "thumbnail",
+        "is_headline",
+        "tags",
+        "status",
+        "published_date",
+        "user_id",
+        "post_category_id"
+    ];
+
+
+    protected $casts = [
+        "tags"           => "array",
+        "published_date" => "date"  
+    ];
+
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->hasOne(PostCategory::class);
+    }
+
+    public function published(){
+        return $this->where('status', '=' ,'Published')->all();
+    }
+
+    public function un_published(){
+        return $this->where('status', '=', 'Unpublished')->all();
+    }
+
+    public function drafts(){
+        return $this->where('status', '=', 'draft')->all();
+    }
+
+}
