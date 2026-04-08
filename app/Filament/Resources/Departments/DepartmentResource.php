9dfbeecec822f8fbfe175e0cae530f64782cb0b7
jsvlr <josedalanon630@gmail.com>
2026-04-08 16:52:40 +0800

    Group the departments to HR

--- app/Filament/Resources/Departments/DepartmentResource.php
+++ app/Filament/Resources/Departments/DepartmentResource.php
@@ -13,6 +13,7 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class DepartmentResource extends Resource
 {
@@ -21,7 +22,7 @@ class DepartmentResource extends Resource
     protected static string|BackedEnum|null $navigationIcon = Heroicon::QueueList;
 
     protected static ?string $recordTitleAttribute = 'name';
-
+    protected static string|UnitEnum|null $navigationGroup = "HR";
     public static function form(Schema $schema): Schema
     {
         return DepartmentForm::configure($schema);
